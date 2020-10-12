1dfb08e5984105ee995bf39832017e6cfa79d622
Ethan Bao <ebao@zagmail.gonzaga.edu>
2020-10-12 16:40:37 -0700

    hello added

--- src/edu/gonzaga/Main.java
+++ src/edu/gonzaga/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String args[]) {
         System.out.println("Starting app");
         Adder adder = new Adder(10, 20);
-
+        System.out.println("hello");
         System.out.println("App finished.");
     }
 }
